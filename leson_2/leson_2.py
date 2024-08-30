1ea8652012b241e37aef8acc79ef1deb2a461730
Kalinichenko Mykhailo <kalinichencomixail@gmail.com>
2024-08-30 20:50:00 +0200

    lesson_2.py added math.py added

---
+++ leson_2/leson_2.py
@@ -0,0 +1,7 @@
+print('How mach it cost')
+price = float(input())
+discount = float(input('Witch discount do you have?: '))
+print(discount)
+print(price)
+final_price = price - discount
+print( 'your final price: ', final_price)
---
+++ leson_2/math.py
@@ -0,0 +1,19 @@
+first_variable = 99
+second_variable = 10
+
+print(first_variable - second_variable)
+print(first_variable + second_variable)
+print(first_variable * second_variable)
+print(first_variable / second_variable)
+
+print()
+
+print(first_variable + second_variable * first_variable)
+
+result_1 = first_variable + second_variable * second_variable
+result_2 = (first_variable + second_variable) * second_variable
+print(result_1, "= result_1")
+print(result_2, '= result_2')
+
+float_1 = 88.99
+print(type(float_1), float_1)
