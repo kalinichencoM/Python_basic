a8517294c6d22f807193444d445278fd36b296fc
Kalinichenko Mykhailo <kalinichencomixail@gmail.com>
2024-08-30 10:15:59 +0200

    lesson_1.py added

---
+++ lesson_1.py
@@ -0,0 +1,13 @@
+var_1 = 10
+
+var_1 = 'Hello!'
+
+print(var_1)
+
+var_str = 'Hello World!'
+var_int = 1987
+type_var_str = type(var_str)
+type_var_int = type(var_int)
+
+print(type_var_str)
+print(type_var_int)
